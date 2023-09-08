c23e90f00d9851762c8172e1e832b9551d339671
Ashraf Magdy <ashrafmagdy926@gmail.com>
2023-09-09 01:45:39 +0300

    added middleware

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ import express from "express";
 //const express = require("express");
 import bodyparser from "body-parser";
 import cors from "cors";
+import serveIndex from "serve-index";
 let port = 3000;
 const app = express();
 app.use(cors());
 
 app.use(bodyparser.urlencoded());
 app.use(bodyparser.json());
-app.use(express.static("website"));
+app.use("/static", express.static("website"));
+app.use("/website", serveIndex("website"));
 app.listen(port, () => console.log("server running on:" + port));
 app.use((req, res, next) => {
   console.log(`time : ${new Date()}`);
